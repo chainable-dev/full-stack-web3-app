25d94536c32d163ca0b4a0ef20268f5890077af0
Souriya Khaosanga <sour@chainable.co>
2023-03-08 21:10:00 -0800

    Using torous

--- application/pages/index.js
+++ application/pages/index.js
@@ -1,5 +1,9 @@
 import {ConnectWallet, useContract, useContractRead, Web3Button} from "@thirdweb-dev/react";
 import React, { useState } from "react";
+import Web3 from "web3";
+import wordmark from "../public/favicon.ico";
+import Torus from "@toruslabs/torus-embed";
+
 
 // Your smart contract address here
 const contractAddress = "0xB08BD1aa7Ee2291c60CE7FfcA4A070Fe2c5936F5";
@@ -19,19 +23,46 @@ export default function Home() {
   // Store the new greeting the user enters in the input in state
   const [newGreeting, setNewGreeting] = useState("");
 
+    const [account, setAccount] = useState();
+
+    const onClickLogin = async (e) => {
+        e.preventDefault();
+
+        const torus = new Torus({});
+        await torus.init({
+            enableLogging: false,
+        });
+        await torus.login();
+
+        const web3 = new Web3(torus.provider);
+        const address = (await web3.eth.getAccounts())[0];
+        const balance = await web3.eth.getBalance(address);
+        setAccount({ address, balance });
+    };
+
   return (
     <div>
-
-
-        <div
-            style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
-        >
-            <h1>Chainable Fintech Company</h1>
-
-
-            <ConnectWallet accentColor="#426CB4" colorMode="dark" />
-
-
+        <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+            <header >
+                {account ? (
+                    <div >
+                        <p>
+                            <strong>Address</strong>: {account.address}
+                        </p>
+                        <p>
+                            <strong>Balance</strong>: {account.balance}
+                        </p>
+                    </div>
+                ) : (
+                    <>
+                        {/* eslint-disable-next-line react/no-unescaped-entities */}
+                        <p>You didn't login yet. Login to see your account details.</p>
+                        <button className="App-link" onClick={onClickLogin}>
+                            Login
+                        </button>
+                    </>
+                )}
+            </header>
         </div>
     </div>
   );
